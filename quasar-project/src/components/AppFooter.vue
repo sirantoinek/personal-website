2710f3d705aa75a7d60305edd572896fad47efec
sirantoinek <sirantoine.kevin@ufl.edu>
2026-01-27 23:59:49 -0500

    style: add initial footer and update page spacing

---
+++ quasar-project/src/components/AppFooter.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+  import pineapple_cat from '../assets/pixel_pineapple_cat.png';
+</script>
+
+<template>
+  <footer class="footer">
+    <img :src="pineapple_cat" alt="pineapple cat pfp" class="footer-photo"/>
+    <p class="footer-text">
+      © 2026 Kevin Sirantoine
+      <br>All Rights Reserved
+    </p>
+  </footer>
+</template>
+
+<style lang="scss">
+  .footer {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 15rem;
+    padding-bottom: 5rem;
+    gap: 0.5rem;
+  }
+
+  .footer-text {
+    text-align: center;
+    color: $lm_dim;
+  }
+  
+  .footer-photo {
+    width: 50px;
+    height: 50px;
+  }
+</style>
--- quasar-project/src/layouts/MainLayout.vue
+++ quasar-project/src/layouts/MainLayout.vue
@@ -1,37 +1,39 @@
 <script setup lang="ts">
   import NavBar from 'components/NavBar.vue';
   import AboutMe from 'components/AboutMe.vue';
+  import AppFooter from 'components/AppFooter.vue';
 </script>
 
 <template>
   <main class="main">
     <NavBar/>
     <AboutMe/>
+    <AppFooter/>
   </main>
 </template>
 
 <style scoped>
 	.main {
 		height: 1500px;
+    width: 90%;
+    max-width: fit-content;
     justify-content: center;
-    margin-left: 2%;
-    margin-right: 2%;
+    margin-left: auto;
+    margin-right: auto;
     padding-left: 2rem;
     padding-right: 2rem;
     transition: 150ms;
 	}
 
-  @media (min-width: 600px) {
+  @media (max-width: 600px) {
     .main {
-      margin-left: 5%;
-      margin-right: 5%;
+      width: 100%;
     }
   }
 
   @media (min-width: 1200px) {
     .main {
-      margin-left: 10%;
-      margin-right: 10%;
+      width: 85%;
     }
   }
 </style>
